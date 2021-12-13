8b57d08d5b456c27c68963239024ddddb6a3e2f8
skswami91 <skswami91@gmail.com>
2021-12-13 15:36:25 +0530

    Changed to older argocd version

--- argo.tf
+++ argo.tf
@@ -2,9 +2,13 @@
 # Example : Simple Argo CD installation
 # ----------------------------------------------------------------------------------------------------------------------
 
+provider "kubernetes" {
+  version = "~> 1.11.0"
+}
+
 module "argo_cd" {
-  source = "git::https://github.com/runoncloud/terraform-kubernetes-argocd.git?ref=1.1.2"
+  source = "git::https://github.com/runoncloud/terraform-kubernetes-argocd.git?ref=v1.0.0"
 
   namespace       = "argocd"
-  argo_cd_version = "1.8.7"
+  argo_cd_version = "1.5.5"
 }
