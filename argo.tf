5f3ab9f337eac8c1ce5b69e1868a85446a2ce67c
skswami91 <skswami91@gmail.com>
2021-12-13 15:45:05 +0530

    Removed the kubernetes provider line from argo.tf

--- argo.tf
+++ argo.tf
@@ -2,10 +2,6 @@
 # Example : Simple Argo CD installation
 # ----------------------------------------------------------------------------------------------------------------------
 
-provider "kubernetes" {
-  version = "~> 1.11.0"
-}
-
 module "argo_cd" {
   source = "git::https://github.com/runoncloud/terraform-kubernetes-argocd.git?ref=v1.0.0"
 
